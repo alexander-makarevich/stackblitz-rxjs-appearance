06d2333fff37acd2a015b2c7574afd55d992f064
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-30 23:00:17 +0700

    add getCommonStyles

--- src/app/roman.service.ts
+++ src/app/roman.service.ts
@@ -1,28 +1,37 @@
 import {Injectable} from '@angular/core';
 
 import {Subject, Observable, of, combineLatest} from 'rxjs'; 
+import {switchMap, map} from 'rxjs/operators';
 
 type Params = any;
+type CommonStyles = any;
 
 @Injectable()
 export class RomanService {
   private languageSource = new Subject<string>();
   private language$ = this.languageSource.asObservable();
-
-  private pageSource = new Subject<string>();
-  private page$ = this.pageSource.asObservable();
-
   setLanguage(language: string) {
     this.languageSource.next(language);
   }
 
+  private pageSource = new Subject<string>();
+  private page$ = this.pageSource.asObservable();
   setPage(page: string) {
     this.pageSource.next(page);
   }
 
-  public params$ = combineLatest(this.language$, this.page$);
+  // private commonStylesSource = new Subject();
+  // private commonStyles$ = this.commonStylesSource.asObservable();
+  getCommonStyles(): Observable<CommonStyles> {
+    return of({common: 'common'});
+  }
 
-  getParams(language: string, page: string): Observable<Params> {
+  public params$ = combineLatest(this.language$, this.page$).pipe(
+    switchMap(([language, page]) => combineLatest(this.getCommonStyles(), this.getParams(language, page))),
+    map(([commonStyles, params]) => ({...params, ...commonStyles}))
+  );
+
+  getParams(language: string, page: string, ): Observable<Params> {
     return of({language: language, page: page});
   }
 }
